18426365385c52fb968266099bf9ec1b7b695c12
OkokonE <unycokokon@gmail.com>
2022-09-09 00:04:55 +0100

    My first java project - Task 14

---
+++ FirstJavaProject3/src/com/Graham/Main.java
@@ -0,0 +1,9 @@
+package com.Graham;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is my first project");
+    }
+}
